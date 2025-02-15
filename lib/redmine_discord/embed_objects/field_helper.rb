15b7aa0ce3766aaf6801e75951ca66fa5d9d655a
Night Stalker <timurdelimin@gmail.com>
2025-02-15 18:47:10 +0700

    Update separator

--- lib/redmine_discord/embed_objects/field_helper.rb
+++ lib/redmine_discord/embed_objects/field_helper.rb
@@ -3,7 +3,7 @@ require_relative 'embed_field'
 module RedmineDiscord
   module EmbedObjects::FieldHelper
     def self.get_separator_field
-      EmbedObjects::EmbedField.new('---------------------------', "\u200b", false).to_hash
+      EmbedObjects::EmbedField.new("\u200b", "────────────────────────", false).to_hash
     end
   end
 end
