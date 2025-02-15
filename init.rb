0af014b156dd5d17d2cce3a8e7ae0e8f103a2540
Night Stalker <timurdelimin@gmail.com>
2025-02-15 18:11:15 +0700

    update version

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :redmine_discord do
   name 'Redmine Discord plugin'
   author 'kory'
   description 'Send notifications to discord channels in a rich embed format.'
-  version '0.0.2'
+  version '0.0.3'
   url 'https://github.com/kory33/redmine_discord'
   author_url 'https://github.com/kory33'
 
